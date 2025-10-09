a7d690120d4f3443b85aba0733e6d97b45b337c4
kamal-sethi <kamalsethi018@gmail.com>
2025-10-09 14:35:38 +0530

    configured the redux store using redux toolkit

---
+++ client/src/state/index.tsx
@@ -0,0 +1,31 @@
+import {createSlice,PayloadAction} from '@reduxjs/toolkit';
+
+
+export interface InitialStateTypes{
+    isSideBarCollapsed:boolean;
+    isDarkMode:boolean;
+
+}
+
+const initialState:InitialStateTypes={
+    isSideBarCollapsed:false,
+    isDarkMode:false,
+}
+
+export const globalSlice=createSlice({
+    name:'global',
+    initialState,
+    reducers:{
+        setIsSidebarCollapsed:(state,action:PayloadAction<boolean>)=>{
+            state.isSideBarCollapsed=action.payload;
+        },
+        setIsDarkMode:(state,action:PayloadAction<boolean>)=>{
+            state.isDarkMode=action.payload;
+        }
+
+    }
+});
+
+export const{setIsSidebarCollapsed,setIsDarkMode}=globalSlice.actions;
+
+export default globalSlice.reducer;
