7998c9ff3b5b05eb3b4436440e5842bd6f9035a5
kamal-sethi <kamalsethi018@gmail.com>
2025-10-09 13:59:36 +0530

    completed navbar component

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Dashboard from "./dashboard/page";
+import DashboardWrapper from "./dashboardWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <DashboardWrapper>{children}</DashboardWrapper>
       </body>
     </html>
   );
